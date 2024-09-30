e8b6ad365f6055df6237fed8eeba46651a12bb31
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2024-09-30 19:14:42 +0530

    Create PROB3.cpp

---
+++ PROB3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+   
+    private:
+        Person(string name, int age) {
+            this->name = name;
+            this->age = age;
+        }
+        void print_info() {
+            cout << "My name is: " << name << " my age is: " << age << endl;
+        }
+    private:
+        string name;
+        int age;
+};
+
+int main(){
+  Person p1("Swasthik",22);
+  Person p2("kamath",27);
+  p1.print_info();
+  p2.print_info();
+  return 0;
+}
