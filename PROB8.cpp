c9d73f96cbcdce16c4dfbb91f1a299e776fba304
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2024-09-30 19:16:52 +0530

    Create PROB8.cpp

---
+++ PROB8.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+    public:
+        Person(string name, int age) {
+            this->name = name;
+            this->age = age;
+        }
+        void print_info() {
+            cout << "My name is: " << name << " my age is: " << age << endl;
+        }
+    private:
+        string name;
+        int age;
+};
+
+class Student : public Person{
+  public:
+    Student(string name, int age , string department): Person(name,age){
+      this->department = department;
+    }
+    void print_info() {
+      Person::print_info();
+      cout << "Department is: "<< department <<endl;
+    }
+  private:
+    string department;
+};
+
+int main(){
+  Person p1("SWASTHIK",27);
+  Person p2("KAMATH",28);
+  p1.print_info();
+  p2.print_info();
+
+  Student s1("SWASTHIK",23,"ECE");
+  s1.print_info();
+  return 0;
+}
